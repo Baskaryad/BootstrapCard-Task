eed7d7ec7e4e8e608aeb3bd73ce22008994c7005
Baskaryad <baskarsurya0308@gmail.com>
2023-06-12 19:05:44 +0530

    Project Day 1 changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import TopBar from './Components/TopBar';
 import Base from './BasePage/Base';
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/Profile';
 
 
 function App() {
- 
-  return (
-    <div className="App">
-       <Base/>
-    </div>
-  );
-}
 
-export default App;
 
+  const studentData = [
+    {
+    name : "sanjay",
+    age : 25
+    },
+    {
+      name : "ajay",
+      age : 23
+      }
+  ]
+  const [data, setData] = useState(studentData)
+    return (
+  <div className="App">
+    <Dashboard
+     data ={data}
+     setData ={setData}
+    />
+  </div>
+    ); 
+  }
+  
+  
+  export default App;
